0d21f64efa459254bc682dbee7242c67e0fe0560
Alexander Hahn <alexanderhahn@Alexanders-MacBook-Pro.local>
2017-10-19 23:57:42 -0400

    working kind of

---
+++ backend/constructor.js
@@ -0,0 +1,15 @@
+class makeItem {
+
+  constructor(name,checked, id) {
+    this.name = name;
+    this.checked = checked;
+    this.id = id;
+  }
+
+var ret = {
+     "name" : name,
+     "checked" : checked,
+     "id" : id
+   };
+
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/thelist', function(req, res){
+router.get('/list', function(req, res){
 
     // Get a Mongo client to work with the Mongo server
     var MongoClient = mongodb.MongoClient;
 
     // Define where the MongoDB server is
-    var url = 'mongodb://localhost:27017/sampsite';
+    var url = 'mongodb://localhost:27017/grocerylist';
 
     // Connect to the server
     MongoClient.connect(url, function (err, db) {
@@ -24,13 +25,15 @@ router.get('/thelist', function(req, res){
             console.log('Connection established to', url);
 
             // Get the documents collection
-            var collection = db.collection('students');
+            var collection = db.collection('list');
 
             // Find all students
             collection.find({}).toArray(function (err, result) {
                 if (err) {
                     res.send(err);
                 } else if (result.length) {
+                    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+                    res.header("Access-Control-Allow-Methods", "PATCH, POST, GET, PUT, DELETE, OPTIONS");
                     res.send(result)
                 } else {
                     res.send('No documents found');

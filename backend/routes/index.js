bcddb2bddc9cc813ad9a2f7c0d18cd883f339afa
Alexander Hahn <alexanderhahn@Alexanders-MacBook-Pro.local>
2017-10-23 10:13:45 -0400

    Added UI, folder was empty on repo for unknown reason

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,8 +12,6 @@ var itemSchema = new Schema({
 
 var item = mongoose.model('list', itemSchema);
 
-<<<<<<< HEAD
-
 router.get('/', function(req, res) {
     res.send("API root directory")
 });
@@ -57,47 +55,9 @@ router.delete('/list' +'/:id', function(req,res) {
                 res.header("Access-Control-Allow-Credentials", "true");
                 return res.send(err);
             } else {
-                console.log("sucess")
+                console.log("successfully deleted")
             }
         })
     })
 
-=======
-router.get('/list', function(req, res){
-
-    // Get a Mongo client to work with the Mongo server
-    var MongoClient = mongodb.MongoClient;
-
-    // Define where the MongoDB server is
-    var url = 'mongodb://localhost:27017/grocerylist';
-
-    // Connect to the server
-    MongoClient.connect(url, function (err, db) {
-        if (err) {
-            console.log('Unable to connect to the Server', err);
-        } else {
-            // We are connected
-            console.log('Connection established to', url);
-
-            // Get the documents collection
-            var collection = db.collection('list');
-
-            // Find all students
-            collection.find({}).toArray(function (err, result) {
-                if (err) {
-                    res.send(err);
-                } else if (result.length) {
-                    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
-                    res.header("Access-Control-Allow-Methods", "PATCH, POST, GET, PUT, DELETE, OPTIONS");
-                    res.send(result)
-                } else {
-                    res.send('No documents found');
-                }
-                //Close connection
-                db.close();
-            });
-        }
-    });
-});
->>>>>>> parent of 3fbc5e0... update
 module.exports = router;
